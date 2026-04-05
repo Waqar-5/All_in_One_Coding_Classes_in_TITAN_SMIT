53223a586315f4f761ee353783232c7a46fbc2bf
Waqar-5 <wa5134810@gmail.com>
2026-04-05 16:03:00 +0500

    added counter by the help of react

---
+++ Assignments/React_All_About/Assignment_02/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App(){
+  return(
+    <>
+    <h1 className="heading">A Simple Counter!</h1>
+    <Counter />
+    </>
+  )
+}
+
+export default App;
