2b642afba8132d4bde42d909857394f48cdca5d0
Waqar-5 <wa5134810@gmail.com>
2026-04-16 10:44:33 +0500

    added Hooks

--- everyday_classes/React_Classes/classes_from_25_March/src/App.jsx
+++ everyday_classes/React_Classes/classes_from_25_March/src/App.jsx
@@ -1,5 +1,7 @@
 
 import "./App.css"; 
+import Both from "./Components/hooks/Both";
+import My_useState from "./Components/hooks/My_useState";
 import All from "./Components/TemplateLiterals/All";
 
 function App(){
@@ -9,6 +11,9 @@ function App(){
   
   {/* Date 14-04-2025 */}
   <All />
+  {/* Date 16-04-2025 */}
+  <My_useState />
+  <Both />
 
 </div>
   </>
